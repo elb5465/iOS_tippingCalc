2cf300c7ed25dd4f2f008b723aba8a3c14dc48c4
Eric Baker <elb5465@gmail.com>
2020-11-26 15:24:47 -0500

    Got tip split working for multiple people with a stepper, and I started to add dark mode.

--- tippingCalc/ViewController.swift
+++ tippingCalc/ViewController.swift
@@ -12,22 +12,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var perPersonLabel: UILabel!
     @IBOutlet weak var tipSegment: UISegmentedControl!
 //    @IBOutlet weak var darkModeSwitch: UISwitch!
     @IBOutlet weak var darkModeSwitch: UISwitch!
-    
+    @IBOutlet weak var numPplLabel: UILabel!
+    @IBOutlet weak var stepperCnt: UIStepper!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    
     @IBAction func onTap(_ sender: Any) {
-        print("hello")
-        
         view.endEditing(true)
-        
+    }
+    
+    
+    @IBAction func updateNumPeople(_ sender: Any) {
+        numPplLabel.text = String(Int(stepperCnt.value))
     }
     
     
@@ -42,10 +45,16 @@ class ViewController: UIViewController {
         
         let tip   = bill * tipSegAmt
         let total = bill + tip
+        var perPerson = total
+
+        if (stepperCnt.value > 0){
+            perPerson = total / Double(stepperCnt.value)
+        }
         
         //update bill and total label
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        tipLabel.text       = String(format: "$%.2f", tip)
+        totalLabel.text     = String(format: "$%.2f", total)
+        perPersonLabel.text = String(format: "$%.2f", perPerson)
     }
     
     @IBAction func toggleDarkMode(_ sender: Any) {
